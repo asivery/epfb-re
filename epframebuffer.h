ed9b79fa195e2c436b4ed3ad19cd8d1266d09150
asivery <asivery@protonmail.com>
2025-02-25 00:38:45 +0100

    Initial commit

---
+++ epframebuffer.h
@@ -0,0 +1,66 @@
+#pragma once
+// This file is missing from the SDK.
+// It's a recreation based on the disassembly of the library.
+#include <QImage>
+#include <QRect>
+#include <QFlags>
+
+#define EPFR_SIZE 0x110
+#define EPFR_OFFSET_MAINBUFFER 0xc8
+#define EPFR_OFFSET_AUXBUFFER 0xa8
+
+enum EPScreenMode {
+    QualityFastest = 0,
+    QualityFast = 1,
+    Quality3 = 3,
+    QualityFull = 4,
+    Quality5 = 5,
+};
+
+enum EPContentType {
+    Mono = 0,
+    Color = 1,
+};
+
+class EPFramebuffer {
+public:
+    enum UpdateFlag {
+        NoRefresh = 0,
+        CompleteRefresh = 1,
+    };
+    void setBuffers(std::tuple<QImage, QImage>, QImage *old=nullptr);
+    unsigned long swapBuffers(QRect param_1, EPContentType epct, EPScreenMode type, QFlags<EPFramebuffer::UpdateFlag> flags);
+    static class EPFramebufferAcep2 *instance();
+    static inline class EPFramebufferAcep2 *nqtInstance();
+};
+
+class EPFramebufferSwtcon : public EPFramebuffer{
+public:
+    void initialize(void);
+    void sync(void);
+    // unsigned long update(QRect param_1, int color, PixelMode type, int fullRefresh);
+};
+
+class EPFramebufferAcep2 : public EPFramebufferSwtcon {
+public:
+    EPFramebufferAcep2();
+    int sendTModeUpdate(void);
+private:
+    char OPAQUE_A[EPFR_OFFSET_AUXBUFFER];
+public:
+    QImage auxBuffer;
+private:
+    char OPAQUE_B[EPFR_OFFSET_MAINBUFFER - sizeof(QImage) - EPFR_OFFSET_AUXBUFFER];
+public:
+    QImage mainBuffer;
+private:
+    char OPAQUE_C[EPFR_SIZE - EPFR_OFFSET_MAINBUFFER - sizeof(QImage)];
+};
+
+class EPFramebufferAcep2 *EPFramebuffer::nqtInstance() {
+    static EPFramebufferAcep2 *fb = nullptr;
+    if(fb == nullptr) {
+        fb = new EPFramebufferAcep2();
+    }
+    return fb;
+}
---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include "epframebuffer.h"
+#include <QImage>
+#include <QPainter>
+#include <QRect>
+#include <QTimer>
+#include <iostream>
+
+int main(int argc, char **argv){
+    EPFramebufferAcep2 *framebuffer = EPFramebuffer::nqtInstance();
+    QImage artwork("/tmp/art.png");
+    QPainter dest(&framebuffer->auxBuffer);
+    dest.drawImage(0, 0, artwork);
+    framebuffer->swapBuffers(QRect(0, 0, artwork.width(), artwork.height()), Color, QualityFastest, EPFramebuffer::UpdateFlag::CompleteRefresh);
+    std::cout << "Buffers swapped" << std::endl;
+    framebuffer->sync();
+    std::cout << "Sync complate!" << std::endl;
+
+    return 0;
+}
